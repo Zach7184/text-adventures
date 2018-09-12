fa36b2018f976ab12334433fa0d90902741a4727
buckldav <runalldayandnight@gmail.com>
2018-09-12 13:31:31 -0600

    Main game loop

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Working")
+print("To start, type 'start'")
+fromUser = input()
+while True:
+    if fromUser == "start":
+        print("Start game!")
+    elif fromUser == "quit": # else if
+        print("Game over!")
+        break # Ends the while loop
+    else:
+        print("To start, type 'start'")
+    fromUser = input()
