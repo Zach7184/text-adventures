ec08c23ae366830b0d63eb833de15991e59163d4
buckldav <runalldayandnight@gmail.com>
2018-09-14 13:39:43 -0600

    Added a verb

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ while True:
     elif fromUser == "quit": # else if
         print("Game over!")
         break # Ends the while loop
+    elif fromUser.split()[0] == "go":
+        print("Go somewhere")
+    elif fromUser == "attack":
+        print("Attack a thing")
     else:
         print("To start, type 'start'")
     fromUser = input()
