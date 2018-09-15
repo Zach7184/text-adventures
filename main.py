2635c01135006427159595ca7cefcf690ed21939
buckldav <runalldayandnight@gmail.com>
2018-09-15 16:59:10 -0600

    Added map

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
-print("To start, type 'start'")
-fromUser = input()
+from adventure import *
+
+# Constants
+START_X = 0
+START_Y = 0
+MAP_X = 2
+MAP_Y = 2
+
+# Variables
+rooms = [[0 for j in range(MAP_Y)] for i in range(MAP_X)]
+rooms[0][0] = Room("Start")
+rooms[0][1] = Room("Forest")
+rooms[1][0] = Room("Lake")
+rooms[1][1] = Room("End")
+
+x = START_X
+y = START_Y
+rooms[x][y]
+
+def go(direction):
+    global x 
+    global y 
+    if direction == 'north':
+        if (y + 1) >= MAP_Y:
+            print("There's a wall there")
+        else:
+            y += 1
+    elif direction == 'south':
+        if (y - 1) < 0:
+            print("There's a wall there")
+        else:
+            y -= 1
+    elif direction == 'west':
+        if (x - 1) < 0:
+            print("There's a wall there")
+        else:
+            x -= 1
+    elif direction == 'east':
+        if (x + 1) >= MAP_X:
+            print("There's a wall there")
+        else:
+            x += 1
+    else:
+        print("I don't understand which direction you're trying to go")
+
 while True:
-    if fromUser == "start":
-        print("Start game!")
-    elif fromUser == "quit": # else if
+    # Print out all the stuff related to the room
+    print(rooms[x][y].name)
+
+    # Get input from the user
+    fromUser = input()
+    if fromUser == "quit":
         print("Game over!")
-        break # Ends the while loop
+        break
     elif fromUser.split()[0] == "go":
-        print("Go somewhere")
-    elif fromUser == "attack":
-        print("Attack a thing")
+        go(fromUser.split()[1])
     else:
         print("To start, type 'start'")
-    fromUser = input()
